36f50e882dc670f428884c3f77ec33c331c2576f
Scott C. <194625444+ScottCameron989@users.noreply.github.com>
2025-02-04 07:41:19 -0500

    Scriptable Object for item Spawn chances

---
+++ Assets/Scripts/WeightedSpawnDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+    [CreateAssetMenu(menuName = "ScriptableObject/WeightedSpawnDB", fileName = "SpawnDB")]
+    public class WeightedSpawnDB : ScriptableObject
+    {
+        [Serializable]
+        internal class SpawnData
+        {
+            [SerializeField]
+            private GameObject _prefabToSpawn;
+            
+            [Range(0f,100f)]
+            [SerializeField]
+            private float _spawnChance;
+            
+            [HideInInspector]
+            public float _weight;
+        }
+        
+        [SerializeField]
+        private SpawnData[] spawnData;
+        
+    }

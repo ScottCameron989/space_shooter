e96bef8291d074ca442260363009a9a9b58ee026
Scott C. <194625444+ScottCameron989@users.noreply.github.com>
2025-02-06 00:10:05 -0500

    Added Camera Shaker Added Shake to camera on player damage

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private Vector3 _position = Vector3.zero;
+    
+    [SerializeField]
+    private float _shakeAmount = .7f;
+    [SerializeField]    
+    private float _decreaseFactor = 1f;
+    
+     
+    private Coroutine _shakeRoutine;
+    private float _shake = 0f;
+    
+    public void Start()
+    {
+        _position = transform.localPosition;
+    }
+    
+    public void StartShake()
+    {
+        _shake = 1;
+    }
+
+    public void Update()
+    {
+        if (_shake > 0)
+        {
+            Vector3 shakePos = Random.insideUnitSphere * _shakeAmount;
+            shakePos.z = _position.z;
+            transform.localPosition = shakePos;
+            _shake -= Time.deltaTime * _decreaseFactor;
+        } else
+        {
+            _shake = 0f;
+            transform.localPosition = _position;
+        }
+    }
+}
